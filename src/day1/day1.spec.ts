3aab020caf95a5e8d0ed0ced486c6ac3b930e45c
stevospinks <github@stephenspinks.com>
2023-12-01 20:34:09 +0000

    Day 1 Part 2

--- src/day1/day1.spec.ts
+++ src/day1/day1.spec.ts
@@ -1,16 +1,24 @@
 import day1 from './index';
 
-const example = `1abc2
+const example1 = `1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet`;
 
+const example2 = `two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen`;
+
 describe('On Day 1', () => {
   it(`part 1 solves for the example input`, () => {
-    expect(day1.solveForPartOne(example)).toBe('142');
+    expect(day1.solveForPartOne(example1)).toBe('142');
   });
 
-  xit(`part 2 solves for the example input`, () => {
-    expect(day1.solveForPartTwo(example)).toBe('hello');
+  it(`part 2 solves for the example input`, () => {
+    expect(day1.solveForPartTwo(example2)).toBe('281');
   });
 });
--- src/day1/index.ts
+++ src/day1/index.ts
@@ -19,7 +19,65 @@ class Day1 extends Day {
   }
 
   solveForPartTwo(input: string): string {
-    return input;
+    const numberMapping = [
+      { number: '1', text: 'one' },
+      { number: '2', text: 'two' },
+      { number: '3', text: 'three' },
+      { number: '4', text: 'four' },
+      { number: '5', text: 'five' },
+      { number: '6', text: 'six' },
+      { number: '7', text: 'seven' },
+      { number: '8', text: 'eight' },
+      { number: '9', text: 'nine' },
+    ];
+    const lines = ImportParser.ToStringArray(input);
+
+    const result = lines.reduce((acc: number, line: string) => {
+      let firstAndLast = '';
+      let numberFound = false;
+      while (!numberFound) {
+        numberMapping.forEach(({ number, text }) => {
+          if (!numberFound && line.startsWith(text)) {
+            numberFound = true;
+            firstAndLast = firstAndLast + number;
+          }
+        });
+
+        if (!numberFound) {
+          const nextChar = line[0];
+          if (!isNaN(parseInt(nextChar))) {
+            numberFound = true;
+            firstAndLast = firstAndLast + nextChar;
+          } else {
+            line = line.slice(1);
+          }
+        }
+      }
+
+      numberFound = false;
+      while (!numberFound) {
+        numberMapping.forEach(({ number, text }) => {
+          if (!numberFound && line.endsWith(text)) {
+            numberFound = true;
+            firstAndLast = firstAndLast + number;
+          }
+        });
+
+        if (!numberFound) {
+          const nextChar = line[line.length - 1];
+          if (!isNaN(parseInt(nextChar))) {
+            numberFound = true;
+            firstAndLast = firstAndLast + nextChar;
+          }
+
+          line = line.slice(0, line.length - 1);
+        }
+      }
+
+      return acc + parseInt(firstAndLast);
+    }, 0);
+
+    return result.toString();
   }
 }
 
